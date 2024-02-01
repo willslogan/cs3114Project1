07e9cb912fb7aea8b69bca2ef7474fd07dbc9527
willslogan <118235237+willslogan@users.noreply.github.com>
2024-02-01 16:22:09 -0500

    laksjdla
    alskdj

---
+++ cs3114Project1/src/SkipListTest.java
@@ -0,0 +1,13 @@
+import student.TestCase;
+
+public class SkipListTest extends TestCase {
+    private SkipList<String, Rectangle> test1 = null;
+    
+    public void setUp() {
+        test1 = new SkipList<String, Rectangle>();
+    }
+    
+    public void InsertTest() {
+        
+    }
+}
